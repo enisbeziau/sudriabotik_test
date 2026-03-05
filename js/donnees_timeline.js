87e7ed2a8ef0f39b3e15872c9102d82a795f62e0
E.B <126325785+enisbeziau@users.noreply.github.com>
2026-03-05 23:53:41 +0100

    Add files via upload

---
+++ js/donnees_timeline.js
@@ -0,0 +1,55 @@
+const lesCoupesDeRobotique = [
+  {
+    date_systeme: "2024-06-01",
+    label_menu: "CR 2024",
+    titre: "Coupe de robotique 2024 - Farming Mars",
+    date_exacte: "08/05/2024",
+    classement: "75 ème",
+    theme: "Le thème de la coupe 2024 est Farming Mars. Les robots ont pour mission de préparer le terrain pour les astronautes arrivant prochainement sur Mars. Ils doivent rempoter des plantes puis les placer dans des serres. Celles-ci seront pollinisées grâce à des coccinelles et alimentées en énergie par des panneaux solaires bien orientés.",
+    president: "Maxime Ringler (promotion 2026)",
+    est_selectionne: true
+  },
+  {
+    date_systeme: "2023-06-01",
+    label_menu: "CR 2023",
+    titre: "Coupe de robotique 2023 - The Cherry on the Cake",
+    date_exacte: "du 17 au 20 mai 2023",
+    classement: "Sudriabotik a été classée 4ème sur 80 équipes homologuées, ce qui est le meilleur classement de l’histoire de l’association.",
+    theme: "Le thème de la coupe 2023 était The Cherry on the Cake. Les robots devaient faire des gâteaux, déposer des cerises dessus et/ou les ranger dans un panier et se déguiser pour faire la fête.",
+    president: "Maxime Ringler (promotion 2026)",
+    est_selectionne: false
+  }
+  ,
+  {
+    date_systeme: "2022-06-01",
+    label_menu: "CR 2023",
+    titre: "Coupe de robotique 2023 - The Cherry on the Cake",
+    date_exacte: "du 17 au 20 mai 2023",
+    classement: "Sudriabotik a été classée 4ème sur 80 équipes homologuées, ce qui est le meilleur classement de l’histoire de l’association.",
+    theme: "Le thème de la coupe 2023 était The Cherry on the Cake. Les robots devaient faire des gâteaux, déposer des cerises dessus et/ou les ranger dans un panier et se déguiser pour faire la fête.",
+    president: "Maxime Ringler (promotion 2026)",
+    est_selectionne: false
+  }
+  ,
+  {
+    date_systeme: "2021-06-01",
+    label_menu: "CR 2023",
+    titre: "Coupe de robotique 2023 - The Cherry on the Cake",
+    date_exacte: "du 17 au 20 mai 2023",
+    classement: "Sudriabotik a été classée 4ème sur 80 équipes homologuées, ce qui est le meilleur classement de l’histoire de l’association.",
+    theme: "Le thème de la coupe 2023 était The Cherry on the Cake. Les robots devaient faire des gâteaux, déposer des cerises dessus et/ou les ranger dans un panier et se déguiser pour faire la fête.",
+    president: "Maxime Ringler (promotion 2026)",
+    est_selectionne: false
+  }
+  ,
+  {
+    date_systeme: "2020-06-01",
+    label_menu: "CR 2023",
+    titre: "Coupe de robotique 2023 - The Cherry on the Cake",
+    date_exacte: "du 17 au 20 mai 2023",
+    classement: "Sudriabotik a été classée 4ème sur 80 équipes homologuées, ce qui est le meilleur classement de l’histoire de l’association.",
+    theme: "Le thème de la coupe 2023 était The Cherry on the Cake. Les robots devaient faire des gâteaux, déposer des cerises dessus et/ou les ranger dans un panier et se déguiser pour faire la fête.",
+    president: "Maxime Ringler (promotion 2026)",
+    est_selectionne: false
+  }
+];
